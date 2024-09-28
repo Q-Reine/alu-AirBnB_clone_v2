c1b2181b5a90e42b24b523ad0e0fdf461fb8c3b5
Q-Reine <q.wihogora@alustudent.com>
2024-09-28 16:26:30 +0200

    Fix:DBStorage - States and Cities

--- models/base_model.py
+++ models/base_model.py
@@ -54,7 +54,9 @@ class BaseModel:
                           (str(type(self)).split('.')[-1]).split('\'')[0]})
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
-        dictionary.pop('_sa_instance_state')
+        dictionary.pop('_sa_instance_state'if '_sa_instance_state' in dictionary.keys():
+        if '_sa_instance_state' in dictionary.keys():
+            del dictionary['_sa_instance_state']
         return dictionary
 
     def delete(self):
