c83d20eefa3ec6cebf0a800d6ece55d7b041b1f1
Q-Reine <q.wihogora@alustudent.com>
2024-10-13 21:11:53 +0200

    Fix

--- models/state.py
+++ models/state.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from sqlalchemy.ext.declarative import declarative_base
-from models.base_model import BaseModel, Base
-from sqlalchemy.orm import relationship
-from sqlalchemy import Column, Integer, String
 import models
+from models.base_model import BaseModel, Base
 from models.city import City
-from os import getenv
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+import os
 
 
 class State(BaseModel, Base):
@@ -15,7 +14,7 @@ class State(BaseModel, Base):
 
     name = Column(String(128), nullable=False)
 
-    if getenv('HBNB_TYPE_STORAGE') == 'db':
+    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
         cities = relationship("City", backref="state",
                               cascade="all, delete, delete-orphan")
 
